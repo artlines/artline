4b289d122bd18afb6a43b193a439be91e43380bd
artur_gazetdinov <artur@gazetdinov.ru>
2017-05-11 23:54:23 +0500

    exclude db.js, edit users model

--- models/users.js
+++ models/users.js
@@ -15,37 +15,3 @@ module.exports.getAll = function(done) {
     done(null, rows)
   })
 }
-var db = require('../db');
-
-module.exports.create = function(userId, text, done) {
-  var values = [userId, text, new Date().toISOString()]
-
-  db.get().query('INSERT INTO comments (user_id, text, date) VALUES(?, ?, ?)', values, function(err, result) {
-    if (err) return done(err);
-    done(null, result.insertId)
-  })
-}
-
-module.exports.getAll = function(done) {
-  db.get().query('SELECT * FROM users', function (err, rows) {
-    if (err) return done(err);
-    done(null, rows)
-  })
-}
-var db = require('../db');
-
-module.exports.create = function(userId, text, done) {
-  var values = [userId, text, new Date().toISOString()]
-
-  db.get().query('INSERT INTO comments (user_id, text, date) VALUES(?, ?, ?)', values, function(err, result) {
-    if (err) return done(err);
-    done(null, result.insertId)
-  })
-}
-
-module.exports.getAll = function(done) {
-  db.get().query('SELECT * FROM users', function (err, rows) {
-    if (err) return done(err);
-    done(null, rows)
-  })
-}
