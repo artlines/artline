714610ede9b332261955e106aef860aa0e226e50
artlines <artur@gazetdinov.ru>
2017-05-22 12:33:07 +0500

    request to json

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -6,10 +6,10 @@ const userCreateSubmit = userCreateForm.userCreateSubmit;
 userCreateSubmit.addEventListener('click',(e) => {
   let stopSubmit = false;
   let inputs = userCreateForm.elements;
-  let data = new FormData();
+  let data = {};
   for (let i = 0, l = inputs.length; i < l; i++) {
-    let input = inputs[i];
 
+    let input = inputs[i];
     if (input.name === 'userCreateSubmit') continue;
     // Проверим валидность поля
     if (input.checkValidity() == false) {
@@ -19,15 +19,18 @@ userCreateSubmit.addEventListener('click',(e) => {
       input.insertAdjacentHTML('afterend', '<p class="error-message">' + customValidityMessageForHTML + '</p>')
       stopSubmit = true;
     }else{
-      data.append(input.name, input.value);
+      data[input.name] = input.value;
     }
+
   }
 
   if (stopSubmit) {
     e.preventDefault();
   }else {
     let xhr = new XMLHttpRequest();
+    let dataJSON = JSON.stringify(data);
     xhr.open("POST", "users/create");
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.onreadystatechange = () => {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
         userCreateForm.insertAdjacentHTML('afterend', '<p class="error-message">' + xhr.responseText + '</p>')
@@ -35,7 +38,7 @@ userCreateSubmit.addEventListener('click',(e) => {
       ;
     }
 
-    xhr.send(data);
+    xhr.send(dataJSON);
   };
 });
 
