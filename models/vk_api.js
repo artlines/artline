a8a50341a52110b457375a2e9b63019462cd3e23
artlines <luart004@gmail.com>
2017-05-27 19:22:23 +0500

    Update vk_api.js

--- models/vk_api.js
+++ models/vk_api.js
@@ -1,5 +1,4 @@
 module.exports = (method, options, done) => {
-  const TOKEN = '0b979af4f3f169267c44c6065b22c7cba881f7dde0fb655db440ca4163660c0a67f28fe83d8e271218a84';
   const URL = 'https://api.vk.com/method/';
   const API_VER = '5.64';
   const https = require('https');
